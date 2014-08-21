f2f8a0b261a02af029887fd4ad8cf043ebd868b1
stkmallu <stk_vtk@yahoo.com>
2014-08-20 18:16:00 -0700

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -58,3 +58,90 @@ main()
         }
     }
 }
+
+void printlist(struct node *head)
+{
+    printf("\n");
+    while(head != NULL)
+    {
+        printf("%d\n",head->data);
+        head = head->next;
+    }
+}
+
+void add(struct node **head, int num)
+{
+    struct node *node = (struct node *)malloc(sizeof(struct node));
+    node->data = num;
+
+    node->next = *head;
+    *head = node;
+}
+
+void delete(struct node **head, int num)
+{
+    struct node *temp = *head;
+    struct node *prev = NULL;
+    if(temp != NULL && temp->data == num)
+    {
+        *head = (*head)->next;
+        free(temp);
+        return;
+    }
+
+    while(temp != NULL && temp->data != num)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if(temp == NULL)
+        return;
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+void printMiddle(struct node *head)
+{
+struct node *slow_ptr = head;
+struct node *fast_ptr = head;
+
+while(fast_ptr != NULL && fast_ptr->next != NULL)
+{
+    slow_ptr = slow_ptr->next;
+    fast_ptr = fast_ptr->next->next;
+
+}
+
+printf("\nMiddle Element = %d\n",slow_ptr->data);
+}
+
+int printNth(struct node *head, int found, int n)
+{
+    struct node *ref1 = head;
+    struct node *ref2 = head;
+
+    int len = 0;
+
+    while (ref1 != NULL && len < n)
+    {
+        ref1 = ref1->next;
+        len++;
+    }
+
+    if(len != n)
+    {
+        printf("\nThe list does not have enough number of elements\n");
+        return;
+    }
+
+    while(ref1 != NULL)
+    {
+        ref1 = ref1->next;
+        ref2 = ref2->next;
+    }
+
+printf("\nThe %d-th element is %d\n",n,ref2->data); */
+
+}
